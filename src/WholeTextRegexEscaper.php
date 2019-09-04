0758cc0a7cdf1f7dad5378c130250965aac7eaba
mauretto78 <maurocassani1978@gmail.com>
2019-09-04 15:55:07 +0200

    Small refactoring

--- src/WholeTextRegexEscaper.php
+++ src/WholeTextRegexEscaper.php
@@ -13,40 +13,55 @@ class WholeTextRegexEscaper
     {
         $escapedNeedle = preg_quote($needle);
         $escapedNeedle = str_replace("/", "\/", $escapedNeedle);
-
         $splittedNeedle = mb_str_split($escapedNeedle);
-
-        $first = '';
-        for ($i=0; $i < count($splittedNeedle); $i++) {
-            if (self::isBoundary($splittedNeedle[$i])) {
-                $first = $i;
-                break;
-            }
-        }
-
-        $last = '';
-        for ($i=(count($splittedNeedle)-1); $i >= 0; $i--) {
-            if (self::isBoundary($splittedNeedle[$i])) {
-                $last = $i;
-                break;
-            }
-        }
 
         $final = '';
 
         foreach ($splittedNeedle as $index => $letter) {
-            if ($index === $first) {
+            if ($index === self::getFirstBounduaryPosition($splittedNeedle)) {
                 $final .= "\\b";
             }
 
             $final .= $letter;
 
-            if ($index === $last) {
+            if ($index === self::getLastBounduaryPosition($splittedNeedle)) {
                 $final .= "\\b";
             }
         }
 
         return $final;
+    }
+
+    /**
+     * @param array $splittedNeedle
+     *
+     * @return int
+     */
+    private static function getFirstBounduaryPosition($splittedNeedle)
+    {
+        for ($i=0; $i < count($splittedNeedle); $i++) {
+            if (self::isBoundary($splittedNeedle[$i])) {
+                return $i;
+            }
+        }
+
+        return -1;
+    }
+
+    /**
+     * @param array $splittedNeedle
+     *
+     * @return int
+     */
+    private static function getLastBounduaryPosition($splittedNeedle)
+    {
+        for ($i=(count($splittedNeedle)-1); $i >= 0; $i--) {
+            if (self::isBoundary($splittedNeedle[$i])) {
+                return $i;
+            }
+        }
+
+        return -1;
     }
 
     /**
@@ -56,6 +71,6 @@ class WholeTextRegexEscaper
      */
     private static function isBoundary($letter)
     {
-        return preg_match("/[A-Za-z0-9_]/", $letter);
+        return ( preg_match("/[A-Za-z0-9_]/", $letter) > 0 ) ? true : false;
     }
 }
